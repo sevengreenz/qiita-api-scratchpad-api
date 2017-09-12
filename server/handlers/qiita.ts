a1dd78b0c9f6cfaffbb33a87aeef60981757c173
sevengreenz <banchikichi+sevengreenz@gmail.com>
2017-09-12 23:33:58 +0900

    first commit

---
+++ server/handlers/qiita.ts
@@ -0,0 +1,17 @@
+import * as lambda from "aws-lambda";
+
+export const hello: lambda.ProxyHandler = async (
+  event: lambda.APIGatewayEvent,
+  context: lambda.Context,
+  callback: lambda.Callback,
+): Promise<void> => {
+  const response = {
+    body: JSON.stringify({
+      message: "Hello TypeScript!!!!",
+    }),
+    headers: {},
+    statusCode: 200,
+  };
+
+  callback(null, response);
+};
