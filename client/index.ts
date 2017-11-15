87924d03ee2c1e05bbeca48945b8a15b864a7398
sevengreenz <banchikichi+sevengreenz@gmail.com>
2017-11-16 01:18:58 +0900

    index.ts から分離して App.vue 実装

--- client/index.ts
+++ client/index.ts
@@ -1,48 +1,8 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import router from './infrastructures/routes';
+import App from './infrastructures/App.vue';
 
 Vue.use(Vuetify);
 
-const vm = new Vue({
-  router,
-  template: `
-    <div>
-       <v-app>
-         <v-toolbar class="light-green" dark fixed>
-           <v-toolbar-title>Qiita API Scratchpad</v-toolbar-title>
-         </v-toolbar>
-         <main>
-           <v-container fluid>
-             <router-view></router-view>
-           </v-container>
-         </main>
-         <v-footer class="light-green"></v-footer>
-       </v-app>
-   </div>`,
-});
+const vm = new App();
 vm.$mount('#app');
-// new Vue({
-//   router,
-//   el: '#app',
-//   template: `
-//     <div>
-//         <v-app>
-//           <v-toolbar class="light-green" dark fixed>
-//             <v-toolbar-title>Qiita API Scratchpad</v-toolbar-title>
-//           </v-toolbar>
-//           <main>
-//             <v-container fluid>
-//               <hello-component :name="name" :initialEnthusiasm="5" />
-//             </v-container>
-//           </main>
-//           <v-footer class="light-green"></v-footer>
-//         </v-app>
-//     </div>`,
-//   data: {
-//     name: 'World',
-//   },
-//   components: {
-//     HelloComponent,
-//   },
-// });
