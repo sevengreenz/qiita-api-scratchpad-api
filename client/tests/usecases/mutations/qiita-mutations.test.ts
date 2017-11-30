12188f6eb2d66e3cfcea6bdd4f53a26b4c2619a6
sevengreenz <banchikichi+sevengreenz@gmail.com>
2017-12-01 01:58:46 +0900

    add mutation のテスト実装

---
+++ client/tests/usecases/mutations/qiita-mutations.test.ts
@@ -0,0 +1,23 @@
+import * as assert from 'assert';
+import qiitaMutations from '../../../usecases/mutations/qiita-mutations';
+import qiitaDomain, { IQiitaState, IApiResponse } from '../../../domain/qiita';
+
+describe('qiita mutations', () => {
+  let state: IQiitaState;
+
+  beforeEach(() => {
+    state = qiitaDomain.createInitialState();
+  });
+
+  it('setApiResponse', () => {
+    const apiResponse: IApiResponse = {
+      headers: 'headers',
+      data: 'data',
+    };
+
+    qiitaMutations.setApiResponse(state, apiResponse);
+
+    assert.equal(state.apiResponse, apiResponse);
+  });
+
+});
