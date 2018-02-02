2511f3bb461c7292d83ce933071fb29bd8340552
sevengreenz <banchikichi+sevengreenz@gmail.com>
2018-02-03 01:25:40 +0900

    modify [client] test のコードを読み込むよう修正

--- client/tests/data/repositories/token-repository.test.ts
+++ client/tests/data/repositories/token-repository.test.ts
@@ -1,30 +1,55 @@
+jest.mock('../../../data/repositories/data-stores/token/token-data-store-factory');
+
 import tokenRepository from '../../../data/repositories/token-repository';
 import { ITokenDataStore } from '../../../data/repositories/data-stores/token/token-data-store-interface';
-import { IDataStoreFactory } from '../../../data/repositories/data-stores/data-store-factory-interface';
+import dataStoreFactory from '../../../data/repositories/data-stores/token/token-data-store-factory';
 
 describe('tokenRepository', () => {
+  const update = jest.fn(() => { });
+
   const mockTokenDataStore: ITokenDataStore = {
+    update,
     issue: (token) => {
       return new Promise<string>((resolve) => {
         resolve(token);
       });
     },
     find: () => 'found_token',
-    update: () => { },
   };
 
-  const mockTokenDataFactory: IDataStoreFactory<ITokenDataStore> = {
-    createCloudDataStore: () => mockTokenDataStore,
-    createLocalDataStore: () => mockTokenDataStore,
-  };
+  dataStoreFactory.createCloudDataStore = jest.fn(() => mockTokenDataStore);
+  dataStoreFactory.createLocalDataStore = jest.fn(() => mockTokenDataStore);
 
-  it('issue should return expected', () => {
-
+  describe('issue', () => {
     const expected = 'token';
+    const result = tokenRepository.issue(expected);
 
-    expect(
-      tokenRepository(mockTokenDataFactory)
-        .issue(expected),
-    ).resolves.toBe(expected);
+    it('should return expected', () => {
+      expect(result).resolves.toBe(expected);
+    });
+
+    it('should createCloudDataStore called', () => {
+      expect(dataStoreFactory.createCloudDataStore).toBeCalled();
+    });
+  });
+
+  describe('find', () => {
+    const result = tokenRepository.find();
+
+    it('should return expected', () => {
+      expect(result).toBe('found_token');
+    });
+
+    it('should createLocalDataStore called', () => {
+      expect(dataStoreFactory.createLocalDataStore).toBeCalled();
+    });
+  });
+
+  describe('update', () => {
+    tokenRepository.update('token');
+
+    it('should be called update function', () => {
+      expect(update).toBeCalled();
+    });
   });
 });
